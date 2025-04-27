fee3831d68bc819af0cf1d5254529587941dc90f
markti <markti@outlook.com>
2025-04-26 20:33:15 -0400

    add ghrunner core

---
+++ root-folder.tf
@@ -0,0 +1,23 @@
+
+locals {
+  files = [
+    ".github/workflows/atat-manual-docker-build.yaml",
+    "src/docker/Dockerfile",
+    "src/docker/entrypoint.sh"
+  ]
+}
+
+resource "github_repository_file" "bulk" {
+
+  for_each = toset(local.files)
+
+  repository          = var.repository
+  branch              = var.branch
+  file                = each.key
+  content             = file("${path.module}/files/${each.key}")
+  commit_message      = "Managed by Terraform"
+  commit_author       = var.commit_user.name
+  commit_email        = var.commit_user.email
+  overwrite_on_create = true
+
+}
---
+++ terraform-folder.tf
@@ -0,0 +1,44 @@
+locals {
+  root_folder_files = [
+    "Dockerfile",
+    "main.tf",
+    "network.tf",
+    "variables.tf",
+    "versions.tf"
+  ]
+}
+
+resource "github_repository_file" "terraform_folder" {
+
+  count = length(local.root_folder_files)
+
+  repository          = var.repository
+  branch              = var.branch
+  file                = "${var.path}/terraform/${local.root_folder_files[count.index]}"
+  content             = file("${path.module}/files/terraform/${local.root_folder_files[count.index]}.t4")
+  commit_message      = "Managed by Terraform"
+  commit_author       = var.commit_user.name
+  commit_email        = var.commit_user.email
+  overwrite_on_create = true
+
+}
+
+resource "github_repository_file" "terraform_tfvars" {
+
+  repository = var.repository
+  branch     = var.branch
+  file       = "${var.path}/terraform/terraform.tfvars"
+  content = templatefile("${path.module}/files/terraform/terraform.tfvars.t4",
+    {
+      primary_location   = var.primary_location
+      base_address_space = var.base_address_space
+      vpn_address_space  = var.vpn_address_space
+      dns_address_space  = var.dns_address_space
+    }
+  )
+  commit_message      = "Managed by Terraform"
+  commit_author       = var.commit_user.name
+  commit_email        = var.commit_user.email
+  overwrite_on_create = true
+
+}
