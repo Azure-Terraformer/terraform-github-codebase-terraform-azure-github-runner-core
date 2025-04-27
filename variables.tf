5a52d4ac9ab701dc2cb7f786ec8050d3362773be
markti <markti@outlook.com>
2025-04-26 20:53:18 -0400

    fix path

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ DESCRIPTION
 }
 variable "path" {
   type        = string
-  default     = "src/terraform"
+  default     = "src"
   description = <<DESCRIPTION
 The file system path within the repository where the Terraform configuration files and workflows will be stored.
 
